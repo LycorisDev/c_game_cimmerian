8c7c09cc469e3041e28da6346f6ae52813f31791
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2025-04-29 22:49:24 +0200

    Place door at an offset

--- src/graphics/raycasting/dda_add_to_list.c
+++ src/graphics/raycasting/dda_add_to_list.c
@@ -1,7 +1,9 @@
 #include "cimmerian.h"
 
 static int	is_cell_see_through(t_map *m, t_ray *r);
-static void	set_perp_wall_dist(t_man *man, t_ray *r, int *add_to_list);
+static void	set_perp_wall_dist(t_man *man, t_ray *r, t_cell *cell,
+				int *add_to_list);
+static int	set_perp_wall_dist_for_door(t_man *man, t_ray *r);
 
 int	dda_add_to_list(t_man *man, t_map *m, t_ray *r, double *max_height)
 {
@@ -18,7 +20,7 @@ int	dda_add_to_list(t_man *man, t_map *m, t_ray *r, double *max_height)
 	if (cell->is_visible)
 	{
 		r->is_see_through = is_cell_see_through(m, r);
-		if (r->is_see_through)
+		if (r->is_see_through || cell->is_door)
 			add_to_list = 1;
 		else if (cell->height > *max_height)
 		{
@@ -26,7 +28,7 @@ int	dda_add_to_list(t_man *man, t_map *m, t_ray *r, double *max_height)
 			add_to_list = 1;
 		}
 		if (add_to_list)
-			set_perp_wall_dist(man, r, &add_to_list);
+			set_perp_wall_dist(man, r, cell, &add_to_list);
 	}
 	return (add_to_list);
 }
@@ -51,13 +53,46 @@ static int	is_cell_see_through(t_map *m, t_ray *r)
 	return (tex->is_see_through[tex->cycle_index]);
 }
 
-static void	set_perp_wall_dist(t_man *man, t_ray *r, int *add_to_list)
+static void	set_perp_wall_dist(t_man *man, t_ray *r, t_cell *cell,
+	int *add_to_list)
 {
-	if (r->side == 0)
-		r->perp_wall_dist = (r->side_dist.x - r->delta_dist.x);
+	if (!cell->is_door)
+	{
+		if (r->side == 0)
+			r->perp_wall_dist = r->side_dist.x - r->delta_dist.x;
+		else
+			r->perp_wall_dist = r->side_dist.y - r->delta_dist.y;
+	}
 	else
-		r->perp_wall_dist = (r->side_dist.y - r->delta_dist.y);
-	if (r->perp_wall_dist > man->dof)
+		*add_to_list = set_perp_wall_dist_for_door(man, r);
+	if (*add_to_list && r->perp_wall_dist > man->dof)
 		*add_to_list = -1;
 	return ;
+}
+
+static int	set_perp_wall_dist_for_door(t_man *man, t_ray *r)
+{
+	t_vec2	wall;
+	double	offset;
+
+	offset = 0.5;
+	if (r->side == 0)
+	{
+		wall.x = r->m_index.x + offset;
+		wall.y = man->player.pos.y + (wall.x - man->player.pos.x)
+			* (r->ray_dir.y / r->ray_dir.x);
+		if (wall.y < r->m_index.y || wall.y > r->m_index.y + 1)
+			return (0);
+		r->perp_wall_dist = (wall.x - man->player.pos.x) / r->ray_dir.x;
+	}
+	else
+	{
+		wall.y = r->m_index.y + offset;
+		wall.x = man->player.pos.x + (wall.y - man->player.pos.y)
+			* (r->ray_dir.x / r->ray_dir.y);
+		if (wall.x < r->m_index.x || wall.x > r->m_index.x + 1)
+			return (0);
+		r->perp_wall_dist = (wall.y - man->player.pos.y) / r->ray_dir.y;
+	}
+	return (1);
 }
--- src/level/extract_sprites.c
+++ src/level/extract_sprites.c
@@ -30,7 +30,6 @@ int	extract_sprites(t_man *man, t_map *map)
 sprite_soul_idle:
 20.5, 11.5
 18.5,  4.5
-10.0,  4.5
 10.0, 12.5
  3.5,  6.5
  3.5, 20.5
@@ -124,19 +123,18 @@ static void	set_soul_sprites(t_spr **spr, t_img *img)
 	int	i;
 
 	i = 0;
-	while (i < 8)
+	while (i < 7)
 	{
 		spr[i]->img = img;
 		++i;
 	}
 	set_vec2(&spr[0]->pos, 20.5, 11.5);
 	set_vec2(&spr[1]->pos, 18.5, 4.5);
-	set_vec2(&spr[2]->pos, 10.0, 4.5);
-	set_vec2(&spr[3]->pos, 10.0, 12.5);
-	set_vec2(&spr[4]->pos, 3.5, 6.5);
-	set_vec2(&spr[5]->pos, 3.5, 20.5);
-	set_vec2(&spr[6]->pos, 3.5, 14.5);
-	set_vec2(&spr[7]->pos, 14.5, 20.5);
+	set_vec2(&spr[2]->pos, 10.0, 12.5);
+	set_vec2(&spr[3]->pos, 3.5, 6.5);
+	set_vec2(&spr[4]->pos, 3.5, 20.5);
+	set_vec2(&spr[5]->pos, 3.5, 14.5);
+	set_vec2(&spr[6]->pos, 14.5, 20.5);
 	if (img && !strcmp(img->id, SPRITE_TYPE_TO_COLLECT))
 	{
 		spr[0]->is_collectible = 1;
@@ -146,7 +144,6 @@ static void	set_soul_sprites(t_spr **spr, t_img *img)
 		spr[4]->is_collectible = 1;
 		spr[5]->is_collectible = 1;
 		spr[6]->is_collectible = 1;
-		spr[7]->is_collectible = 1;
 	}
 	return ;
 }
@@ -156,15 +153,15 @@ static void	set_pillar_sprites(t_spr **spr, t_img *img)
 	int	i;
 
 	i = 8;
-	while (i < 12)
+	while (i < 11)
 	{
 		spr[i]->img = img;
 		++i;
 	}
-	set_vec2(&spr[8]->pos, 18.5, 10.5);
-	set_vec2(&spr[9]->pos, 18.5, 11.5);
-	set_vec2(&spr[10]->pos, 18.5, 12.5);
-	set_vec2(&spr[11]->pos, 8.5, 7.0);
+	set_vec2(&spr[7]->pos, 18.5, 10.5);
+	set_vec2(&spr[8]->pos, 18.5, 11.5);
+	set_vec2(&spr[9]->pos, 18.5, 12.5);
+	set_vec2(&spr[10]->pos, 8.5, 7.0);
 	return ;
 }
 
@@ -173,18 +170,18 @@ static void	set_barrel_sprites(t_spr **spr, t_img *img)
 	int	i;
 
 	i = 12;
-	while (i < 20)
+	while (i < 19)
 	{
 		spr[i]->img = img;
 		++i;
 	}
-	set_vec2(&spr[12]->pos, 21.5, 1.5);
-	set_vec2(&spr[13]->pos, 15.5, 1.5);
-	set_vec2(&spr[14]->pos, 16.0, 1.8);
-	set_vec2(&spr[15]->pos, 16.2, 1.2);
-	set_vec2(&spr[16]->pos, 3.5, 2.5);
-	set_vec2(&spr[17]->pos, 9.5, 15.5);
-	set_vec2(&spr[18]->pos, 10.0, 15.1);
-	set_vec2(&spr[19]->pos, 10.5, 15.8);
+	set_vec2(&spr[11]->pos, 21.5, 1.5);
+	set_vec2(&spr[12]->pos, 15.5, 1.5);
+	set_vec2(&spr[13]->pos, 16.0, 1.8);
+	set_vec2(&spr[14]->pos, 16.2, 1.2);
+	set_vec2(&spr[15]->pos, 3.5, 2.5);
+	set_vec2(&spr[16]->pos, 9.5, 15.5);
+	set_vec2(&spr[17]->pos, 10.0, 15.1);
+	set_vec2(&spr[18]->pos, 10.5, 15.8);
 	return ;
 }
